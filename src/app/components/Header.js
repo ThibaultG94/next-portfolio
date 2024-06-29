717de22b2c3123415500f40955aab64cad7d0d2c
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-06-29 02:31:12 +0200

    chore: Update profile image in header component

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import ThemeToggle from "./ThemeToggle";
 import { Menu, X } from "lucide-react";
+import Image from "next/image";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,16 +14,20 @@ const Header = () => {
   return (
     <div className="container mx-auto flex justify-between items-center p-4">
       <div className="flex items-center">
-        <img
-          src="/path-to-your-logo.png"
-          alt="profil"
-          className="h-8 w-8 mr-2"
-        />
+        <div className="h-11 w-11 sm:h-12 sm:w-12 md:h-14 md:w-14 lg:h-16 lg:w-16 mr-2 rounded-full overflow-hidden relative">
+          <Image
+            src="/img/profil.jpg"
+            alt="profil"
+            width={80}
+            height={111}
+            className="absolute top-0 left-0 transform translate-y-[-4px]"
+          />
+        </div>
         <span className="text-xl font-bold text-gray-900 dark:text-gray-100">
           Thibault Guilhem
         </span>
       </div>
-      <nav className="hidden md:flex space-x-4">
+      <nav className="hidden md:flex items-center space-x-4">
         <a
           href="#home"
           className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400"
