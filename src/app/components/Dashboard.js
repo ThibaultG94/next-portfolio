4b02ea53bd0ef6a853457d660d2c5a2fbf095395
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-06-29 19:57:52 +0200

    chore: Add react-icons package and update Dashboard component

--- src/app/components/Dashboard.js
+++ src/app/components/Dashboard.js
@@ -3,6 +3,7 @@
 
 import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 
 const Dashboard = () => {
   const [text] = useTypewriter({
@@ -18,17 +19,33 @@ const Dashboard = () => {
   });
 
   return (
-    <section className="h-screen flex flex-col justify-center items-center bg-dark text-light">
-      <h1 className="text-5xl">Bonjour 👋</h1>
-      <h2 className="text-3xl mt-4">Je suis Thibault Guilhem</h2>
-      <p className="text-2xl my-2">
+    <section className="h-screen flex flex-col justify-center items-center bg-dark text-center text-light">
+      <h1 className="text-xl sm:text-2xl">Bonjour 👋</h1>
+      <h2 className="text-xl sm:text-2xl mt-2">Je suis Thibault Guilhem</h2>
+      <p className="text-2xl sm:text-3xl my-4">
         {text}
         <Cursor />
       </p>
-      <div>Mes réseaux</div>
-      <div className="mt-4 flex space-x-4">
-        <button className="btn-primary">CV</button>
-        <button className="btn-secondary">Contactez-moi</button>
+      <div className="mt-4 flex space-x-6">
+        <a
+          href="https://www.linkedin.com/in/thibault-g-10b37a271/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600"
+        >
+          <FaLinkedin size={30} />
+        </a>
+        <a
+          href="https://github.com/ThibaultG94"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-800 dark:text-white"
+        >
+          <FaGithub size={30} />
+        </a>
+        <a href="mailto:thibault.guilhem@gmail.com" className="text-gray-900">
+          <FaEnvelope size={30} />
+        </a>
       </div>
     </section>
   );
