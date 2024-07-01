148d25f78cdfeb46e77ee20a18d5fd9aafdccb03
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-07-01 16:16:18 +0200

    chore: Add embla-carousel-react dependency and update app.scss styles

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -1,8 +1,8 @@
-// components/Projects.tsx
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import useEmblaCarousel from "embla-carousel-react";
 
 const projets = [
   {
@@ -21,6 +21,7 @@ const projets = [
       "/img/task-manager/task-manager-taskspage.jpg",
     ],
     github: "https://github.com/ThibaultG94/Task-manager-frontend",
+    url: "https://task-manager-front.admin.serv.ndplugins.com/",
   },
   {
     title: "Thiblog",
@@ -33,35 +34,30 @@ const projets = [
       "/img/quasar-blog/quasar-blog-about.jpg",
     ],
     github: "https://github.com/ThibaultG94/quasar-blog",
+    url: "https://github.com/ThibaultG94/quasar-blog",
   },
 ];
 
 const Projects = () => {
   const [currentProject, setCurrentProject] = useState(0);
-  const [currentImage, setCurrentImage] = useState(0);
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    loop: false,
+    duration: 15,
+  });
+
+  useEffect(() => {
+    if (emblaApi) {
+      emblaApi.reInit();
+      emblaApi.scrollTo(0);
+    }
+  }, [currentProject, emblaApi]);
 
   const nextProject = () => {
     setCurrentProject((prev) => (prev + 1) % projets.length);
-    setCurrentImage(0); // Reset image index when changing project
   };
 
   const prevProject = () => {
     setCurrentProject((prev) => (prev - 1 + projets.length) % projets.length);
-    setCurrentImage(0); // Reset image index when changing project
-  };
-
-  const nextImage = () => {
-    setCurrentImage(
-      (prev) => (prev + 1) % projets[currentProject].images.length
-    );
-  };
-
-  const prevImage = () => {
-    setCurrentImage(
-      (prev) =>
-        (prev - 1 + projets[currentProject].images.length) %
-        projets[currentProject].images.length
-    );
   };
 
   return (
@@ -74,37 +70,55 @@ const Projects = () => {
       <div className="mt-12 flex justify-center items-center space-x-4">
         <div className="relative">
           <div className="laptop">
-            <button
-              onClick={prevImage}
-              className="absolute left-1 top-1/2 transform -translate-y-1/2 text-xs text-gray-800 dark:text-gray-400 hover:text-gray-200 transition duration-300 z-10 bg-white rounded-full p-0.5"
-            >
-              <FaArrowLeft />
-            </button>
-            <div className="laptop__screen">
-              <img
-                src={projets[currentProject].images[currentImage]}
-                alt={projets[currentProject].title}
-              />
+            {emblaApi && emblaApi.canScrollPrev() && (
+              <button
+                onClick={() => emblaApi.scrollPrev()}
+                className="absolute left-1 top-1/2 transform -translate-y-1/2 text-xs text-gray-800 dark:text-gray-400 hover:text-gray-200 transition duration-10 z-10 bg-white rounded-full p-0.5"
+              >
+                <FaArrowLeft />
+              </button>
+            )}
+            <div className="overflow-hidden laptop__screen" ref={emblaRef}>
+              <div className="flex">
+                {projets[currentProject].images.map((image, index) => (
+                  <div
+                    className={`min-w-full relative z-0 ${
+                      index === 0
+                        ? "ml-0 mr-6"
+                        : index === projets[currentProject].images.length - 1
+                        ? "ml-6 mr-0"
+                        : "ml-6 mr-6"
+                    }`}
+                    key={index}
+                  >
+                    <img src={image} alt={projets[currentProject].title} />
+                  </div>
+                ))}
+              </div>
             </div>
             <div className="laptop__bottom">
               <div className="laptop__under"></div>
             </div>
             <div className="laptop__shadow"></div>
-            <button
-              onClick={nextImage}
-              className="absolute right-1 top-1/2 transform -translate-y-1/2 text-xs text-gray-800 dark:text-gray-400 hover:text-gray-200 transition duration-300 z-10 bg-white rounded-full p-0.5"
-            >
-              <FaArrowRight />
-            </button>
+            {emblaApi && emblaApi.canScrollNext() && (
+              <button
+                onClick={() => emblaApi.scrollNext()}
+                className="absolute right-1 top-1/2 transform -translate-y-1/2 text-xs text-gray-800 dark:text-gray-400 hover:text-gray-200 transition duration-300 z-10 bg-white rounded-full p-0.5"
+              >
+                <FaArrowRight />
+              </button>
+            )}
           </div>
         </div>
         <div className="relative tablet">
-          <button
-            onClick={prevProject}
-            className="absolute left-8 top-1/2 transform -translate-y-1/2 text-2xl text-gray-800 dark:text-gray-400 hover:text-gray-200 transition duration-300 z-10"
-          >
-            <FaArrowLeft />
-          </button>
+          {currentProject > 0 && (
+            <button
+              onClick={prevProject}
+              className="absolute left-8 top-1/2 transform -translate-y-1/2 text-2xl text-gray-800 dark:text-gray-400 hover:text-gray-200 transition duration-300 z-10"
+            >
+              <FaArrowLeft />
+            </button>
+          )}
           <div className="tablet__screen bg-white dark:bg-black">
             <div className="tablet__content">
               <h3 className="text-2xl font-semibold">
@@ -119,14 +133,24 @@ const Projects = () => {
               >
                 Voir sur GitHub
               </a>
+              <a
+                href={projets[currentProject].url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mt-2 text-blue-500 hover:text-blue-400"
+              >
+                Voir le site
+              </a>
             </div>
           </div>
-          <button
-            onClick={nextProject}
-            className="absolute right-8 top-1/2 transform -translate-y-1/2 text-2xl text-gray-800 dark:text-gray-400 hover:text-gray-200 transition duration-300 z-10"
-          >
-            <FaArrowRight />
-          </button>
+          {currentProject < projets.length - 1 && (
+            <button
+              onClick={nextProject}
+              className="absolute right-8 top-1/2 transform -translate-y-1/2 text-2xl text-gray-800 dark:text-gray-400 hover:text-gray-200 transition duration-300 z-10"
+            >
+              <FaArrowRight />
+            </button>
+          )}
         </div>
       </div>
     </section>
