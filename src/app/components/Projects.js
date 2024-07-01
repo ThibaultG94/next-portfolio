38b8117804b8af3f7a7d07eeeb373c492cfd7d72
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-07-01 20:43:43 +0200

    Update Projects component with new tablet view and navigation functionality

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -39,6 +39,7 @@ const projets = [
       "/img/quasar-blog/thiblog-projects-dark.jpg",
       "/img/quasar-blog/thiblog-about-dark.jpg",
     ],
+    tabletImages: ["/img/quasar-blog/thiblog-home-tablet.jpg"],
     github: "https://github.com/ThibaultG94/quasar-blog",
     url: "https://github.com/ThibaultG94/quasar-blog",
   },
@@ -81,6 +82,8 @@ const Projects = () => {
       ? projets[currentProject].darkImages
       : projets[currentProject].images;
 
+  const tabletImages = projets[currentProject].tabletImages || currentImages;
+
   return (
     <section className="py-20 bg-dark text-light">
       <h2 className="text-5xl text-center font-bold">Mes Travaux</h2>
@@ -88,8 +91,9 @@ const Projects = () => {
         J'ai développé plusieurs projets fullstack from scratch dont voici les
         principaux.
       </p>
-      <div className="mt-12 flex justify-center items-center space-x-4">
-        <div className="relative">
+      <div className="mt-12 flex flex-wrap justify-center items-center space-x-4">
+        {/* Laptop view for larger screens */}
+        <div className="hidden md:block relative">
           <div className="laptop">
             <button
               onClick={prevImage}
@@ -115,14 +119,39 @@ const Projects = () => {
             </button>
           </div>
         </div>
-        <div className="relative tablet">
+        {/* Tablet view for smaller screens */}
+        <div className="md:hidden relative">
+          <div className="tablet">
+            <button
+              onClick={prevImage}
+              className="absolute left-1 top-1/2 transform -translate-y-1/2 text-xs text-gray-800 dark:text-gray-400 hover:text-gray-200 transition duration-300 z-10 bg-white rounded-full p-0.5"
+            >
+              <FaArrowLeft />
+            </button>
+            <div className="tablet__screen">
+              <img
+                src={tabletImages[currentImage]}
+                alt={projets[currentProject].title}
+              />
+            </div>
+            <div className="tablet__shadow"></div>
+            <button
+              onClick={nextImage}
+              className="absolute right-1 top-1/2 transform -translate-y-1/2 text-xs text-gray-800 dark:text-gray-400 hover:text-gray-200 transition duration-300 z-10 bg-white rounded-full p-0.5"
+            >
+              <FaArrowRight />
+            </button>
+          </div>
+        </div>
+        {/* Tablet view for project info */}
+        <div className="relative tablet pt-10 xl:pt-0">
           <button
             onClick={prevProject}
             className="absolute left-8 top-1/2 transform -translate-y-1/2 text-2xl text-gray-800 dark:text-gray-400 hover:text-gray-200 transition duration-300 z-10"
           >
             <FaArrowLeft />
           </button>
-          <div className="tablet__screen bg-white dark:bg-black">
+          <div className="tablet__screen bg-white dark:bg-black p-8">
             <div className="tablet__content">
               <h3 className="text-2xl font-semibold">
                 {projets[currentProject].title}
