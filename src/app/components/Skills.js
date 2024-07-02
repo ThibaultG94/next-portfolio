b01b690580b09ce3b79b272259f3a0eb1a3a8684
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-07-02 11:58:18 +0200

    chore: Update Projects component with new project images and navigation functionality

--- src/app/components/Skills.js
+++ src/app/components/Skills.js
@@ -9,9 +9,23 @@ const Skills = () => {
         esthétique en utilisant une architecture frontend moderne.
       </p>
       <div className="mt-8 flex justify-center space-x-4">
-        <img src="/path/to/js.png" alt="JavaScript" className="w-16 h-16" />
-        <img src="/path/to/react.png" alt="React" className="w-16 h-16" />
-        <img src="/path/to/next.png" alt="Next.js" className="w-16 h-16" />
+        <img src="/img/logos/js.png" alt="JavaScript" className="w-16 h-16" />
+        <img src="/img/logos/react.png" alt="React" className="w-16 h-14" />
+        <img src="/img/logos/next-js.svg" alt="Next.js" className="w-16 h-16" />
+        <img src="/img/logos/quasar.svg" alt="Quasarjs" className="w-16 h-16" />
+        <img
+          src="/img/logos/tailwind-css.svg"
+          alt="Tailwind CSS"
+          className="w-20 h-14"
+        />
+        <img src="/img/logos/sass.png" alt="SASS" className="w-20 h-14" />
+        <img src="/img/logos/css.png" alt="CSS3" className="w-16 h-16" />
+        <img
+          src="/img/logos/html.png"
+          alt="HTML5"
+          className="w-14 h-14 my-auto"
+        />
+        <img src="/img/logos/nodejs.png" alt="Node js" className="w-16 h-16" />
         {/* Ajoute d'autres icônes selon les compétences */}
       </div>
     </section>
