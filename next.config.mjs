c368ad0084c9e73bbe03167d2121cb8ffa74abcc
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-12-28 16:56:51 +0100

    Ajouter la configuration pour désactiver les messages de compilation à la demande en production et filtrer les logs des fetches d'images dans le composant OptimizedImage.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,18 @@ const nextConfig = {
   images: {
     formats: ["image/webp"],
   },
+  onDemandEntries: {
+    // Désactive les messages de compilation à la demande en production
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
+  // Fonction pour filtrer les logs
+  logging: {
+    fetches: {
+      // Ne log pas les fetches d'images en production
+      fullUrl: process.env.NODE_ENV === "production" ? false : true,
+    },
+  },
 };
 
 export default nextConfig;
