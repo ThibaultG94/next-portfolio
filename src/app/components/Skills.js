908a18af48267b90109266de41a02ae87821b839
Thibault <thibault.guilhem@gmail.com>
2025-02-12 00:09:41 +0100

    Improved responsiveness and styling of the Skills component; adjustment of image sizes and CSS classes for better adaptation to screens.

--- src/app/components/Skills.js
+++ src/app/components/Skills.js
@@ -4,74 +4,74 @@ import Image from "next/image";
 const Skills = () => {
   return (
     <section className="py-10 sm:py-12 md:py-16 lg:py-20 px-3">
-      <h2 className="text-3xl text-center">Mes Compétences</h2>
-      <p className="text-xl text-center mt-4">
+      <h2 className="text-3xl 2xl:text-4xl text-center">Mes Compétences</h2>
+      <p className="text-xl 2xl:text-2xl text-center mt-4 xl:mt-5 2xl:mt-6">
         J&apos;aime prendre la responsabilité de créer une expérience
         utilisateur esthétique en utilisant une architecture frontend moderne.
       </p>
-      <div className="mt-8 flex flex-wrap justify-around space-x-4 space-y-2 max-w-sm sm:max-w-md md:max-w-xl lg:max-w-5xl mx-auto">
+      <div className="mt-8 xl:mt-9 2xl:mt-10 flex flex-wrap justify-around items-center space-x-4 space-y-2 max-w-sm sm:max-w-md md:max-w-xl lg:max-w-5xl mx-auto">
         <Image
           src="/img/logos/js.png"
           alt="JavaScript"
           width={64}
           height={64}
-          className="w-16 h-16"
+          className="w-auto h-16"
         />
         <Image
           src="/img/logos/react.png"
           alt="React"
-          width={64}
-          height={56}
-          className="w-16 h-14"
+          width={86}
+          height={75}
+          className="w-auto h-16"
         />
         <Image
           src="/img/logos/next-js.svg"
           alt="Next.js"
           width={64}
           height={64}
-          className="w-16 h-16"
+          className="w-auto h-6"
         />
         <Image
           src="/img/logos/quasar.svg"
           alt="Quasarjs"
           width={64}
           height={64}
-          className="w-16 h-16"
+          className="w-auto h-16"
         />
         <Image
           src="/img/logos/tailwind-css.svg"
           alt="Tailwind CSS"
           width={80}
           height={56}
-          className="w-20 h-14"
+          className="w-auto h-16"
         />
         <Image
           src="/img/logos/sass.png"
           alt="SASS"
           width={80}
           height={56}
-          className="w-20 h-14"
+          className="w-auto h-16"
         />
         <Image
           src="/img/logos/css.png"
           alt="CSS3"
           width={64}
           height={64}
-          className="w-16 h-16"
+          className="w-auto h-16"
         />
         <Image
           src="/img/logos/html.png"
           alt="HTML5"
           width={56}
           height={56}
-          className="w-14 h-14 my-auto"
+          className="w-auto h-14"
         />
         <Image
           src="/img/logos/nodejs.png"
           alt="Node js"
           width={64}
           height={64}
-          className="w-16 h-16"
+          className="w-auto h-18"
         />
       </div>
     </section>
