8a0aecc4588046c4f756cfd1a1fd0714e4b0e7e8
Thibault <thibault.guilhem@gmail.com>
2025-02-11 22:44:06 +0100

    Improved structure and style of Footer component for better presentation and responsiveness; adjustment of sizes and spacing for better adaptation to different screens.

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -11,15 +11,17 @@ const Footer = () => {
   }, []);
 
   return (
-    <footer className="min-h-screen flex flex-col justify-end items-center dark:bg-gray-900 py-20">
-      <div className="w-full max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-12 px-6">
-        <div className="text-center md:text-left">
-          <h3 className="text-xl font-semibold mb-6">À propos</h3>
-          <p className="text-gray-600 dark:text-gray-300 mb-6">
+    <footer className="min-h-[50vh] lg:min-h-screen md:min-h-[70vh] sm:min-h-[60vh] flex flex-col justify-end py-28">
+      <div className="w-full max-w-[90rem] mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-12 p-4 sm:p-6 md:p-8 lg:p-10 2xl:p-12">
+        <div className="text-center md:text-left space-y-3 md:space-y-4">
+          <h3 className="text-lg sm:text-xl md:text-2xl font-semibold">
+            À propos
+          </h3>
+          <p className="text-sm sm:text-base md:text-lg text-gray-600 dark:text-gray-300">
             Je développe des sites web modernes en utilisant une architecture
             frontend moderne et réactive.
           </p>
-          <div className="flex justify-center md:justify-start space-x-6">
+          <div className="flex justify-center md:justify-start space-x-4 md:space-x-6">
             <a
               href="https://www.linkedin.com/in/thibault-g-10b37a271/"
               target="_blank"
@@ -48,13 +50,14 @@ const Footer = () => {
           </div>
         </div>
 
-        {/* Navigation */}
         <div className="text-center md:text-left">
-          <h3 className="text-xl font-semibold mb-6">Navigation</h3>
-          <nav className="flex flex-col space-y-4">
+          <h3 className="text-lg sm:text-xl md:text-2xl font-semibold mb-3 md:mb-4">
+            Navigation
+          </h3>
+          <nav className="flex flex-col space-y-2 md:space-y-3">
             <a
               href="#dashboard"
-              className="text-gray-600 dark:text-gray-300 hover:text-blue-500 transition-colors"
+              className="text-sm sm:text-base md:text-lg text-gray-600 dark:text-gray-300 hover:text-blue-500"
             >
               Accueil
             </a>
@@ -79,37 +82,37 @@ const Footer = () => {
           </nav>
         </div>
 
-        {/* Technologies */}
         <div className="text-center md:text-left">
-          <h3 className="text-xl font-semibold mb-6">Technologies</h3>
-          <div className="flex justify-center md:justify-start items-center space-x-8">
+          <h3 className="text-lg sm:text-xl md:text-2xl font-semibold mb-3 md:mb-6">
+            Technologies
+          </h3>
+          <div className="grid grid-cols-4 gap-2 sm:gap-3 md:gap-4 justify-items-center md:justify-items-start">
             <img
               src="/img/logos/js.png"
               alt="JavaScript"
-              className="w-10 h-10 object-contain"
+              className="w-8 h-auto sm:w-10 md:w-12 my-auto"
             />
             <img
               src="/img/logos/react.png"
               alt="React"
-              className="w-10 h-10 object-contain"
+              className="w-8 h-auto sm:w-10 md:w-12 my-auto"
             />
             <img
               src="/img/logos/next-js.svg"
               alt="Next.js"
-              className="w-10 h-10 object-contain"
+              className="w-8 h-auto sm:w-10 md:w-12 my-auto"
             />
             <img
               src="/img/logos/nodejs.png"
               alt="Node.js"
-              className="w-10 h-10 object-contain"
+              className="w-8 h-auto sm:w-10 md:w-12 my-auto"
             />
           </div>
         </div>
       </div>
 
-      {/* Copyright */}
-      <div className="w-3/4 mt-20 pt-8 border-t border-gray-200 dark:border-gray-700">
-        <p className="text-center text-gray-500 dark:text-gray-400">
+      <div className="w-1/2 mx-auto mt-6 sm:mt-8 md:mt-10 pt-4 sm:pt-6 border-t border-gray-200 dark:border-gray-700">
+        <p className="text-center text-xs sm:text-sm md:text-base text-gray-500 dark:text-gray-400 py-4">
           © {new Date().getFullYear()} Thibault Guilhem. Tous droits réservés.
         </p>
       </div>
