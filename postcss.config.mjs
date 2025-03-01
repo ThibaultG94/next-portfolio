2f32c2b78171f9a23dc4f804706a31e48ba13b42
Thibault Guilhem <thibault.guilhem@gmail.com>
2025-03-01 12:23:01 +0100

    Updated PostCSS configuration to enable dark mode and extend themes with new backgrounds and breakpoints.

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,29 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    "@tailwindcss/postcss": {
+      darkMode: "selector",
+      content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+      ],
+      safelist: ["dark:bg-[#121212]/50"],
+      theme: {
+        extend: {
+          backgroundImage: {
+            "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+            "gradient-conic":
+              "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          },
+          screens: {
+            xs: "425px",
+            xxs: "350px",
+          },
+        },
+      },
+      plugins: [],
+    },
   },
 };
 
